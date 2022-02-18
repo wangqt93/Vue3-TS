c9f48f986867d507af5c8e84d45e973d684df513
wangqt <969909031@qq.com>
2022-02-18 23:56:42 +0800

    ref和reactive

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <MyTest></MyTest>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import MyTest from './components/MyTest.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyTest
   }
 });
 </script>
---
+++ src/components/MyTest.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>{{num}}</h1>
+        <h2>{{numtwo}}</h2>
+        <button v-on:click="add">+1</button>
+    </div>
+</template>
+
+<script lang='ts'>
+
+import { defineComponent,computed,reactive,toRefs } from 'vue'
+interface DataProps {
+    num: number,
+    // add: ()=> void,
+    // numtwo: number
+}
+export default defineComponent({
+    name: 'MyTest',
+    setup() {
+        // const num = ref(1)  //通过ref，定义vue的状态
+        // const add = () => {  //自定义方法，vue2写在methos对象当中
+        //     console.log('11')
+        //    num.value ++
+        // }
+        // const numtwo = computed(()=>{   // computed计算属性
+        //     return num.value *2
+        // })
+        // return {
+        //     num,
+        //     add,
+        //     numtwo
+        // }
+        const data: DataProps = reactive({
+            num: 0
+        })
+        const add = () => {
+            data.num ++
+        }
+        const numtwo = computed(() => {
+            return data.num *2
+        })
+
+        const dataRef = toRefs(data)
+        return {
+            ...dataRef,
+            add,
+            numtwo
+        }
+    },
+})
+</script>
