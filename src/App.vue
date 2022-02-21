8ce20088f16f2dff6338ba502d452d20386ddb40
Z问号 <7673629+wangqt2020@user.noreply.gitee.com>
2022-02-21 10:47:16 +0800

    fsd

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="hello"/>
   <TestOne name='wangqt' />
+  <Teleportde />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 import TestOne from './components/Test/one.vue'
+import Teleportde from './TS_DEMO/Teleport.vue'
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
-    TestOne
+    TestOne,
+    Teleportde
   }
 });
 </script>
---
+++ src/TS_DEMO/Teleport.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>import 'ant-design-vue/dist/antd.css';</p>
+    </div>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Teleport',
+    setup() {
+        return  {
+
+        }
+    },
+})
+</script>
