6a1183e7ee100f6d8a7c3e8d2bcd37582c91a81f
Z问号 <7673629+wangqt2020@user.noreply.gitee.com>
2022-02-21 10:57:22 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="hello"/>
-  <TestOne name='wangqt' />
-  <Teleportde />
+    <div>
+      <h1>哈哈</h1>
+    </div>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import TestOne from './components/Test/one.vue'
-import Teleportde from './TS_DEMO/Teleport.vue'
+
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
-    TestOne,
-    Teleportde
   }
 });
 </script>
